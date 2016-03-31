412bb2b21319c77e720cecf3816b48b5acbc6f74
Lusindiso <lusindiso@projectcodex.co>
2016-03-31 12:41:40 +0200

    lusindiso

---
+++ avo.js
@@ -0,0 +1,18 @@
+var avos = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50."
+
+//use split method to make my string an Array
+
+var newAvos = avos.split(",");
+// console.log(newAvos);
+
+//use the split method to remove the for in my Array;
+var avos2 = avos.split("for")
+//
+// console.log(avos2);
+
+//use a forEach loop to calculate the price per Avo
+  avos2.forEach(function(avos2){
+
+});
+
+ console.log(avos2);
